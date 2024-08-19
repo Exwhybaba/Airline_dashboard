f21ae37b52ef2bc2a65e9872bf0134bd26b36481
Exwhybaba <seyeoyelayo@gmail.com>
2024-08-19 22:17:18 +0100

    adding the tracking again

--- src/app.py
+++ src/app.py
@@ -120,9 +120,36 @@ import dash
 from dash import dcc, html
 
 # Initialize the Dash app
-app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
+app = dash.Dash(__name__,meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 server = app.server
-
+# Set the custom index string
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Dash App</title>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-28PGC2X4Y3"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-28PGC2X4Y3');
+        </script>
+    </head>
+    <body>
+        <div id="react-entry-point">
+            {%app_entry%}
+        </div>
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
 
 # Define the layout of the app
 app.layout = html.Div(children=[
